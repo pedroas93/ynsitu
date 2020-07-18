8910004cb8677d071819bbc872b4e26881dedb38
david felipe pedraza rubiano <david.pedraza@javeriana.edu.co>
2020-07-18 01:39:27 -0500

    reverse Finiched

--- demo1/vue.config.js
+++ demo1/vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/metronic/vue/demo1/"
+      ? "/ynsitu/"
       : "/",
   configureWebpack: {
     resolve: {
